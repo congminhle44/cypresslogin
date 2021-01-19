902d02e58588e0b2d442040daad775dad9334162
congminhle44 <46514752+congminhle44@users.noreply.github.com>
2021-01-19 15:10:35 +0700

    refactor: main form refactor

--- src/page/components/Form/index.tsx
+++ src/page/components/Form/index.tsx
@@ -8,87 +8,51 @@ import Button from '../../../components/Button';
 import Forward from '../../../components/icons/forward';
 
 import Input from '../../../components/Input';
+import { emailPattern } from '../../../helpers/pattern';
+import switchErrors from '../../../validation';
 
 import styles from './form.module.css';
 
-interface Props {}
+interface Props {
+  submitEvent?: any;
+}
 
 type Inputs = {
   email?: string;
   password?: string;
 };
 
-const Form: FC<Props> = (props) => {
+const Form: FC<Props> = ({ submitEvent }) => {
   const { register, handleSubmit, errors } = useForm<Inputs>();
 
-  // eslint-disable-next-line
-  const emailPattern = /^(([^<>()\[\]\\.,;:\s-@#$!%^&*+=_/`?{}|'"]+(\.[^<>()\[\]\\.,;:\s-@_!#$%^&*()=+/`?{}|'"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
-
-  const switchErrors = (inputName: any) => {
-    switch (inputName.type) {
-      case 'required':
-        return 'This field is required';
-      case 'minLength':
-        return "This field doesn't meet the min length";
-      case 'maxLength':
-        return 'This field is out of max length';
-      case 'pattern':
-        return 'Wrong type of email';
-    }
-  };
-
-  const submitEvent = (data: any) => {
-    alert('Login success');
-  };
-
   return (
-    <form onSubmit={handleSubmit(submitEvent)} className={styles.form}>
-      <div className={styles.formHead}>
-        <p className={styles.welcome}>
-          <span className={styles.highlight}>Welcome</span> to sign in
-        </p>
-        <p className={styles.headDescription}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores vel
-          reprehenderit tempore? Quibusdam, iste iusto.
-        </p>
-      </div>
-      <div className={styles.formMain}>
-        <div className={styles.inputWrap}>
-          <div className={styles.email}>
-            <Input
-              type='email'
-              name='email'
-              placeHolder='Type email'
-              refs={register({
-                required: true,
-                minLength: 6,
-                maxLength: 24,
-                pattern: emailPattern,
-              })}
-              error={errors.email && switchErrors(errors.email)}
-            />
-          </div>
-          <div className={styles.password}>
-            <Input
-              type='password'
-              name='password'
-              placeholder='Type password'
-              refs={register({ required: true, minLength: 6, maxLength: 24 })}
-              error={errors.password && switchErrors(errors.password)}
-            />
-          </div>
+    <form onSubmit={handleSubmit(submitEvent)} className={styles.formMain}>
+      <div className={styles.inputWrap}>
+        <div className={styles.email}>
+          <Input
+            type='email'
+            name='email'
+            placeHolder='Type email'
+            refs={register({
+              required: true,
+              minLength: 6,
+              maxLength: 24,
+              pattern: emailPattern,
+            })}
+            error={errors.email && switchErrors(errors.email)}
+          />
         </div>
-        <Button type='submit'>Sign in</Button>
-      </div>
-      <div className={styles.others}>
-        <p className={styles.forgot}>Forgot your password?</p>
-        <div className={styles.signupWrap}>
-          <p className={styles.signup}>Sign up</p>
-          <div className={styles.forward}>
-            <Forward />
-          </div>
+        <div className={styles.password}>
+          <Input
+            type='password'
+            name='password'
+            placeholder='Type password'
+            refs={register({ required: true, minLength: 6, maxLength: 24 })}
+            error={errors.password && switchErrors(errors.password)}
+          />
         </div>
       </div>
+      <Button type='submit'>Sign in</Button>
     </form>
   );
 };
--- src/validation/index.tsx
+++ src/validation/index.tsx
@@ -1,28 +1,16 @@
 /** @format */
 
-const emailValid = (value: string, maxLength: number) => {
-  // eslint-disable-next-line
-  const emailRegex = /^(([^<>()\[\]\\.,;:\s-@#$!%^&*+=_/`?{}|'"]+(\.[^<>()\[\]\\.,;:\s-@_!#$%^&*()=+/`?{}|'"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
-
-  if (value === '') return 'This field can not empty';
-
-  if (value.length < 6) return 'This field required at least 6 characters';
-
-  if (value.length > maxLength)
-    return `This field can not have over ${maxLength} characters`;
-
-  if (!value.match(emailRegex)) return 'Invalid type of email';
-  else return '';
+const switchErrors = (inputName: any) => {
+  switch (inputName.type) {
+    case 'required':
+      return 'This field is required';
+    case 'minLength':
+      return "This field doesn't meet the min length";
+    case 'maxLength':
+      return 'This field is out of max length';
+    case 'pattern':
+      return 'Wrong type of email';
+  }
 };
 
-const passwordValid = (value: string, maxLength: number) => {
-  if (value === '') return 'This field can not empty';
-
-  if (value.length < 6) return 'This field required at least 6 characters';
-
-  if (value.length > maxLength)
-    return `This field can not have over ${maxLength} characters`;
-  else return '';
-};
-
-export { emailValid, passwordValid };
+export default switchErrors;
